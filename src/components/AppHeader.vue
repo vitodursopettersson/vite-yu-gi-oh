f81cf0434db829fd775afedd776d6ca033db02b5
vitodursopettersson <vitodurso98@gmail.com>
2023-12-15 10:57:06 +0100

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <img src="/yugioh-logo.webp" alt="Yu-Gi-Oh logo">
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+<style scoped>
+header {
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+
+img {
+    width: 140px;
+}
+
+h1 {
+    font-weight: 500;
+}
+</style>
