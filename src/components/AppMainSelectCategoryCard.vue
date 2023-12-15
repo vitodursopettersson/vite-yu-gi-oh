3f5e964648070372699e6dfe3acaf8d4318b8550
vitodursopettersson <vitodurso98@gmail.com>
2023-12-15 11:44:11 +0100

    select archetype

---
+++ src/components/AppMainSelectCategoryCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppMainSelectCategoryCard'
+}
+</script>
+
+<template>
+    <select name="archetype" id="#archetype-select">
+        <option value="exodia">Exodia</option>
+        <option value="alien">Alien</option>
+    </select>
+</template>
+<style scoped>
+select {
+    border: 1px solid #dddddd;
+    padding: 8px 20px;
+    border-radius: 4px;
+}
+</style>
